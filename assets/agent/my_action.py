5b5af73a63f9b18e098ed22f77ae39cc4654e131
jiangcan <jiangcan182@gmail.com>
2026-05-06 17:47:09 +0800

    更新

--- assets/agent/my_action.py
+++ assets/agent/my_action.py
@@ -1,6 +1,8 @@
+
+from maa import controller
 from maa.agent.agent_server import AgentServer
-from maa.custom_action import CustomAction
 from maa.context import Context
+from maa.custom_action import CustomAction
 
 
 @AgentServer.custom_action("my_action_111")
@@ -12,6 +14,6 @@ class MyCustomAction(CustomAction):
         argv: CustomAction.RunArg,
     ) -> bool:
 
-        print("my_action_111 is running!")
-
+        controller.post_click(100, 10).wait()  # 执行点击
+        context.override_next(["TaskA", "TaskB"])      # 动态调整任务流
         return True
